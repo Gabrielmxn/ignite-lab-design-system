8c11d6ea89c2689fe14b2b425a2f0b2d29471f48
Gabriel Alves <gabriel.s.t.a.r.k@hotmail.com>
2022-10-13 21:55:13 -0300

    storybook

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
+import { Checkbox } from './components/Checkbox';
 import './styles/global.css';
 
 export function App() {
   return (
-    <h1 className="bg-cyan-500 text-2xl">Hello World</h1>
+   <Checkbox />
   )
 }
 
